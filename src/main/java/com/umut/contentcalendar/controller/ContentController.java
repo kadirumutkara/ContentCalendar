49ecf5a3f819412286488c1177bef36e328c322e
kadirumutkara <kadirumutkara@gmail.com>
2023-12-21 13:30:00 +0300

    Version 2

--- src/main/java/com/umut/contentcalendar/controller/ContentController.java
+++ src/main/java/com/umut/contentcalendar/controller/ContentController.java
@@ -1,14 +1,57 @@
 package com.umut.contentcalendar.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.umut.contentcalendar.model.Content;
+import com.umut.contentcalendar.repository.ContentCollectionRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
 
+    private final ContentCollectionRepository repository;
 
 
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+
+        return repository.findByID(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not found"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id) {
+        if (!repository.existById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found");
+        }
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void update(@PathVariable Integer id) {
+
+        repository.delete(id);
+    }
 
 }
--- src/main/java/com/umut/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/umut/contentcalendar/repository/ContentCollectionRepository.java
@@ -2,16 +2,59 @@ package com.umut.contentcalendar.repository;
 
 
 import com.umut.contentcalendar.model.Content;
+import com.umut.contentcalendar.model.Status;
+import com.umut.contentcalendar.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ContentCollectionRepository {
 
-    private  final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
+
+    public ContentCollectionRepository() {
 
 
+    }
 
+    public List<Content> findAll() {
+        return contentList;
+    }
+
+    public Optional<Content> findByID(Integer id) {
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+
+    public void save(Content content){
+        contentList.removeIf(c -> c.id().equals(content.id()));
+        contentList.add(content);
+    }
+
+
+    @PostConstruct
+    private void init() {
+        Content c = new Content(1,
+                "my first blog",
+                "My first blog",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,null);
+        contentList.add(c);
+    }
+
+    public boolean existById(Integer id) {
+        return contentList.stream().filter(c -> c.id().equals(id)).count()==1;
+    }
+
+    public void delete(Integer id) {
+
+        contentList.removeIf(c->c.id().equals(id));
+    }
 }
